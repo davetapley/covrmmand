b6e5b67ad25b25c9ee9b08aa0fa0a4af280882f7
Dave Tapley <dukedave@gmail.com>
2013-01-26 12:59:21 -0700

    Logging omniauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,5 +1,6 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
+    puts "ACCESS_TOKEN: #{ request.env["omniauth.auth"] }"
     @user = User.find_for_google_oauth2 request.env["omniauth.auth"], current_user
     sign_in_and_redirect @user, event: :authentication
   end
