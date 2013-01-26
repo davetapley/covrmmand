561867d6e1ae78e2b08367eee771a180799a582e
Dave Tapley <dukedave@gmail.com>
2013-01-26 14:11:01 -0700

    Revert "Logging omniauth"
    This reverts commit b6e5b67ad25b25c9ee9b08aa0fa0a4af280882f7.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,5 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
-    puts "ACCESS_TOKEN: #{ request.env["omniauth.auth"] }"
     @user = User.find_for_google_oauth2 request.env["omniauth.auth"], current_user
     sign_in_and_redirect @user, event: :authentication
   end
