ea3f0d159c92182f0415dd19cb44f4397adae1cd
Dave Tapley <dukedave@gmail.com>
2013-01-26 12:24:46 -0700

    Added dashboard

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,7 +1,7 @@
-
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
     @user = User.find_for_google_oauth2 request.env["omniauth.auth"], current_user
     sign_in_and_redirect @user, event: :authentication
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Covrmmand::Application.routes.draw do
+
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   devise_scope :user do
     get 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
   end
+
+  resource :dashboard, only: [:show]
+
+  root to: 'dashboards#show'
 
 end
 
