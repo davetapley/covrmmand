0d426b6d2a86383cf778bc7d76a8a31a29b7e48c
Dave Tapley <dukedave@gmail.com>
2013-01-27 10:43:44 -0700

    Don't attempt to show users who are active, but don't have a location

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -5,7 +5,8 @@ class DashboardsController < ApplicationController
   before_filter :authenticate_user!
 
   def show
-    @gmaps_json = User.where(active: true).to_gmaps4rails do |user, marker|
+    @active_users = User.where(active: true, :location.exists => true)
+    @gmaps_json = @active_users.to_gmaps4rails do |user, marker|
       marker.picture( rich_marker: "<div style='background-color:white'><h1>#{ user.level }</h1>#{ user.name }</div>" )
     end
   end
