3788517aa5316d259860b4148de0cd75f4da2b3b
Dave Tapley <dukedave@gmail.com>
2013-02-01 16:46:40 -0700

    Added location age to marker

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -7,7 +7,7 @@ class DashboardsController < ApplicationController
   def show
     @active_users = User.where(active: true, :location.exists => true)
     @gmaps_json = @active_users.to_gmaps4rails do |user, marker|
-      marker.picture( rich_marker: "<div style='background-color:white'><h1>#{ user.level }</h1>#{ user.name }</div>" )
+      marker.picture( rich_marker: "<div style='background-color:white; width: 150px'><h1>#{ user.level }</h1>#{ user.name }<br/>#{ view_context.time_ago_in_words user.location.timestamp } ago</div>" )
     end
   end
 
